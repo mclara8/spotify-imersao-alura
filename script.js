ad2a23878d5d7a967c70c9cd134ec12357728a57
mclara8 <mariiaclara428@gmail.com>
2025-02-04 01:23:50 -0300

    reorganizando divs no header da página

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById('search-input');
-const resultsArtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
@@ -10,7 +10,7 @@ function requestApi(searchTerm) {
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
+    resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
@@ -19,17 +19,16 @@ function displayResults(result) {
         artistImage.src = element.urlImg;
     });
 
-    resultsArtist.classList.remove('hidden');
+    resultArtist.classList.remove('hidden');
 }
 
-document.addEventListener('input', function() {
+document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-
-    if (searchTerm === ''){
+    if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultsArtist.classList.remove('hidden');
-        return;
+        resultArtist.classList.remove('hidden');
+        return
     }
-
+    
     requestApi(searchTerm);
-});
+})
